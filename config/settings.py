4337f9060aecacc891440352b4b6efc6521f4d0f
gau <gau@1jz.ru>
2024-04-28 18:44:36 +0300

    Создание приложение БОТ и запуск в тестовом режиме

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bot.apps.BotConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TOKEN_BOT = os.getenv('TOKEN_BOT')
